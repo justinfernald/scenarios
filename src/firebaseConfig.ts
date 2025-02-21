789c7d03fa48136d5fc956ecff37c8b248990f95
Justin Fernald <jfernald@my.gcu.edu>
2025-02-21 14:18:11 -0700

    add Firebase Analytics integration to firebaseConfig

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,5 +1,6 @@
 // src/firebase/firebaseConfig.js
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
@@ -15,9 +16,10 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const functions = getFunctions(app);
 const db = getFirestore();
 const googleProvider = new GoogleAuthProvider();
 
-export { auth, db, googleProvider, signInWithPopup, signOut, functions };
+export { auth, analytics, db, googleProvider, signInWithPopup, signOut, functions };
